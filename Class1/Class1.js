cf1de95b33b55e6e8ed6f5cbea5087f5bb62e5df
Carl <carltracy@gmail.com>
2012-09-12 21:37:10 -0400

    init

---
+++ Class1/Class1.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+    
+    <h1>Mad Libs!</h1>
+    <script>
+
+        var answer;
+        var retHTML = '';
+        
+        
+        document.write(retHTML);
+
+    
+    </script>
+
+    </body>
+</html>
