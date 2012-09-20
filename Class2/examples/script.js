1e2732a9031774421ea2e3539d7ee166b0ade654
Alyssa <alydiaz77@gmail.com>
2012-09-20 18:50:36 -0400

    finishing example

--- Class2/examples/script.js
+++ Class2/examples/script.js
@@ -2,14 +2,31 @@
     This put your JavaScript in here!
 */
 
-function validate(element) {
-    if(element.value === "") {
+function validate(field) {
+    if(isEmpty(field)) {
         document.getElementById("errorText").setAttribute("class", "");
-        element.setAttribute("class", "error");
+        field.setAttribute("class", "error");
     } else {
         document.getElementById("errorText").setAttribute("class", "hidden");
-        element.setAttribute("class", "");   
+        field.setAttribute("class", "");   
     }
+}
+
+function isEmpty(field) {
+    return field.value === "";
+}
+
+function writeMadLib() { 
+   var noun1Field = document.getElementById("noun1");
+   var noun2Field = document.getElementById("noun2");
+   
+   if(isEmpty(noun1Field) || isEmpty(noun2Field)) {
+        validate(noun1Field);
+        validate(noun2Field);
+   } else {
+       var resultDiv = document.getElementById("madDiv");
+       resultDiv.innerHTML = "I like " + noun1Field.value + ". Sometimes I even like a good " + noun2Field.value + ".";
+   }
 }
 
 
