ecf5b3baaf46975498d612433ae92d1ddfe82926
Alyssa <alydiaz77@gmail.com>
2012-09-19 20:08:40 -0400

    sytled it up

--- Class2/examples/script.js
+++ Class2/examples/script.js
@@ -2,4 +2,15 @@
     This put your JavaScript in here!
 */
 
+function validate(element) {
+    if(element.value === "") {
+        document.getElementById("errorText").setAttribute("class", "");
+        element.setAttribute("class", "error");
+    } else {
+        document.getElementById("errorText").setAttribute("class", "hidden");
+        element.setAttribute("class", "");   
+    }
+}
+
+
 
