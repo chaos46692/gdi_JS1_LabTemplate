410b7c61669a129501b1977bed405c1c09d1efaa
Carl <carltracy@gmail.com>
2012-10-10 10:35:31 -0400

    added second solution for Class for homework A

---
+++ Class4/homework_solution/homeworkA1/fruit.1.js
@@ -0,0 +1,202 @@
+/*
+    This put your JavaScript in here!
+*/
+
+/*
+    Get the fruit - only query the DOM once to get the "fruit" input
+    once you have it - just return the value 
+*/
+var fruitField;
+
+/* returns the form field */
+function getFruitField() {
+    if ((fruitField === null) || (fruitField === undefined)) {
+        fruitField = document.getElementById("fruit");
+        
+        if ((fruitField === null) || (fruitField === undefined)) {
+            return undefined;
+        }
+    }
+    
+    return fruitField;
+    
+}
+
+/* returns the form value */
+function getFruit() {
+    var ff = getFruitField();
+    if (ff === undefined) {
+        return "I AIN'T GOT NO FRUIT!";
+    }
+    return ff.value;
+    
+}
+
+/*
+    Hook the return key in the field so that hitting "enter" 
+    doesn't submit the form but insted updates the output
+*/
+function checkEnter(e){
+    e = e || event; /* e is the event that's passed from onkeypress */
+    
+    /*
+          This part is just getting the numerical
+          representation of which key was pressed             13 is the key code
+                                                              for 'return'      */
+    if ( (e.keyCode || event.which || event.charCode || 0) == 13 ) {
+        /* they pressed return - update the field
+           and return false which prevents the 
+           form from submitting */
+        updateCost();
+        var ff = getFruitField();
+        ff.select();
+        return false;
+    } else {
+        return true;   /* everything's okay - return true (-> let the key go through) */
+    }
+    
+    /* check out more key codes here:
+        http://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
+    */
+}
+
+function update(fld) {
+       //validate(fruitField);
+       return false;
+    
+}
+
+function updateCost() {
+    var fruit = getFruit();  // fruitField.value.toLowerCase();
+    var output = document.getElementById("cost-out");
+    var bgImg = document.getElementById("fruitCost");
+    var cost = -1.0;
+    var unit = "lb";
+    var img = 20;
+    var wtf = "";
+    
+    /* fruit comomodity prices from
+        http://www.ams.usda.gov/mnreports/fvwretail.pdf
+    */
+    switch (fruit.toLocaleLowerCase()) {
+        case "apple": case "apples":
+            cost = 1.79;
+            img = 1;
+            break;
+        case "honey crisp apple": case "honey crisp apples":
+            cost = 2.11;
+            img = 1;
+            break;
+        case "orange": case "oranges":
+            cost = 3.99;
+            img = 9;
+            break;
+        case "banana": case "bananas":
+            cost = 0.52;
+            img = 2;
+            break;
+        case "blueberry": case "blueberries":
+            unit = "6 oz package";
+            cost = 3.93;
+            img = 3;
+            break;
+        case "kiwi": case "kiwifruit":
+            cost = 0.37;
+            unit = "each";
+            img = 5;
+            break;
+        case "kiwis": case "kiwifruits":
+            img = 5;
+            wtf = "<div class='wtf'>THAT'S NOT HOW YOU PLURALIZE KIWI!</div>";
+            break;
+        case "grapefruit": case "grapefruits":
+            cost = 1.07;
+            unit = "each";
+            img = 4;
+            break;
+        case "lemon": case "lemons":
+            cost = 0.53;
+            unit = "each";
+            img = 6;
+            break;
+        case "lime": case "limes":
+            cost = 0.29;
+            unit = "each";
+            img = 7;
+            break;
+        case "peach": case "peaches":
+            cost = 1.76;
+            img = 10;
+            break;
+        case "strawberry": case "strawberries":
+            cost = 2.70;
+            img = 12;
+            break;
+        case "mango": case "mangoes":
+            cost = 1.29;
+            unit = "each";
+            img = 8;
+            break;
+        case "raspberry": case "raspberries":
+            cost = 2.80;
+            unit = "6 oz package";
+            img = 11;
+            break;
+        case "watermelon": case "watermelons":
+            cost = 1.74;
+            img = 13;
+            break;
+
+/*
+        case "": case "s":
+            cost = ;
+            break;
+*/
+        
+    }
+    if (output) {
+        var s = "";
+        if (wtf === "") {
+            if (cost < 0.0) {
+                hideCost();
+                //outputImg.innerHTML = "I could not find that fruit, sorry!";
+                //return false;
+            } else {
+                s = "$" + cost.toFixed(2) + " / " + unit;  
+                showCost();
+            }
+            
+            
+        } else {
+            s = wtf;
+            showCost();
+            //hideCost();
+            //outputImg.innerHTML = wtf;
+            //return;
+        }
+        output.innerHTML =  s;
+        
+        if (bgImg) {
+            //outputImg.innerHTML = '<img src="' + img + '">';
+            bgImg.style.backgroundPosition = "0px -" + (img-1) * 300 + "px";
+        }
+    }    
+
+}
+
+function showCost() {
+    var cost = document.getElementById("cost-tag");
+    if (cost) {
+        cost.style.display = "block";   
+    }
+}
+
+function hideCost() {
+    var cost = document.getElementById("cost-tag");
+    if (cost) {
+        cost.style.display = "none";
+    }
+}
+
+
+
