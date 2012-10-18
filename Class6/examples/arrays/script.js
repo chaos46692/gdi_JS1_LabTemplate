64be2b92c8772a576ced358d47c1171d023d9fb2
Carl <carltracy@gmail.com>
2012-10-18 16:30:15 -0400

    Update tedate template for class 6

--- Class6/examples/arrays/script.js
+++ Class6/examples/arrays/script.js
@@ -1,19 +1,42 @@
 
 
-function updateOutput() {
-    var elem = document.getElementById('frmMain').elements;
-     
-    var i;
-    var s = '';
-    for(i = 0; i < elem.length; i++) {
-        var eachElem = elem[i];
-        s += '<p>' + eachElem.name + ' = ' + eachElem.value + ' </p>';
+function writeToDiv(id,html) {
+    var output = document.getElementById(id);
+    if (output) {
+        output.innerHTML = html;   
+        return true;
+    } else {
+        return false;   
+    }
+}
+
+function clearDiv(id) {
+    var output = document.getElementById(id);
+    if (output) {
+        output.innerHTML = "";   
+        return true;
+    } else {
+        return false;   
+    }
+}
+
+
+function example() {
+    var fruit = new Array("apple","banana","guava","papaya","tomato","orange","grape");
+    var n;
+    var html = "";
+    
+    for(n = 0; n < fruit.length; n++) {
+        html += "<p>" + fruit[n] + " is a fruit, it's yummy!";   
     }
     
-    var divOutput = document.getElementById("exampleOutput");
-    if (divOutput !== null) {
-        divOutput.innerHTML = s;
-    }  
-    
-     
+    writeToDiv("exampleOutput",html);
 }
+
+
+
+function codeAlongUpdate() {
+    var animals = new Array("cow","pig","cat","horse","dog"); 
+    
+}
+
