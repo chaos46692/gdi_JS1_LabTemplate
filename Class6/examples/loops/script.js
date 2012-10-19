f9cdabb0e177f29cf6c034a2599f974460cffb6d
Carl <carltracy@gmail.com>
2012-10-18 23:56:35 -0400

    updated Class 6 homework / moved material to Class 7

--- Class6/examples/loops/script.js
+++ Class6/examples/loops/script.js
@@ -3,4 +3,30 @@ function processForm() {
 }
 
 function determineAverage() {
+    
+    var max = 5;
+    
+    var result = "<table><tr><td>X</td>";
+    
+    for(var j = 0; j < max; j++) {
+            result = result + "<td>" + j + "</td>";
+    }
+        
+    result = result + "</tr>";
+    
+    for(var i = 0; i < max; i++) {
+        
+        result = result + "<tr><td>" + i + "</td>";
+        
+        for(var k = 0; k < max; k++) {
+            result = result + "<td>" + (k * i) + "</td>";
+        }
+        
+        result = result + "</tr>";
+        
+    }
+    
+    result = result + "</table>";
+    
+    document.getElementById("lab2").innerHTML = result;
 }
